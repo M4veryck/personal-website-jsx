1dd85b70ae317c721effa96d7e8c6936c9bb0721
M4veryck <mmaya@unal.edu.co>
2022-04-24 03:40:49 -0500

    v 1.1.2: No PWA again

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ class MyDocument extends Document {
                         href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700&display=swap"
                         rel="stylesheet"
                     />
-                    <link rel="manifest" href="/manifest.json" />
+                    {/* <link rel="manifest" href="/manifest.json" /> */}
                     <link rel="apple-touch-icon" href="/icon-192x192.png" />
                     <meta name="theme-color" content="#1CB4E2" />
                 </Head>
